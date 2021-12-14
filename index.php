dec876ede4ad1c6a877dff8a738082776cd173f4
ajgali <96140855+ajgali@users.noreply.github.com>
2021-12-14 16:49:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 echo "how are you doing today? my name is aj gali and this is my runcloud github wordpress server.";
-echo ' 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "https://images.wallpapersden.com/image/download/bleach-art-kurosaki-ichigo_a21laJSZmpqtpaSklGdqa2WtZmpsaA.jpg">
-<html>
-<img src="$image_url='https://www.google.com/intl/en_com/images/srpr/logo3w.png';" width="1920" height="1080" title="Kurosaki Ichigo" alt="Kurosaki Ichigo" />
-
-</html>
-';
+$image_url='https://www.google.com/intl/en_com/images/srpr/logo3w.png';';
+<img src="<?php echo $image_url;?>">
 ?>
