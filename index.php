b6f882a6d1a0eb652ca35b005cd47ccc262e539e
ajgali <96140855+ajgali@users.noreply.github.com>
2021-12-14 16:25:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 echo "how are you doing today? my name is aj gali and this is my runcloud github wordpress server.";
+echo ' 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "https://images.wallpapersden.com/image/download/bleach-art-kurosaki-ichigo_a21laJSZmpqtpaSklGdqa2WtZmpsaA.jpg">
+<html>
+<img src="/images/image1.png" width="280" height="125" title="Logo of a company" alt="Kurosaki Ichigo" />
+
+</html>
+';
 ?>
