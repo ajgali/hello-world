d500412912e5e0ebda3146b98d6ec2ac4d2d39df
ajgali <96140855+ajgali@users.noreply.github.com>
2021-12-15 09:00:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo ("Hello");
-        ?>
-        <div>
-            <img src="https://images.wallpapersden.com/image/download/bleach-art-kurosaki-ichigo_a21laJSZmpqtpaSklGdqa2WtZmpsaA.jpg" alt="myPic" />
-        </div>
-    </body>
-</html>
-
-
+<?php
+echo "kallan is a stud
+?>
